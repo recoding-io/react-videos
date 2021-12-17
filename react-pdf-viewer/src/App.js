836f94c013e13f03705f15798501c14bab79194b
Haaris Iqubal <mdhaarisiqubal@icloud.com>
2021-12-17 18:58:20 +0530

    Added React PDF View Project

---
+++ react-pdf-viewer/src/App.js
@@ -0,0 +1,60 @@
+import React, {useState} from 'react'
+import { Document,Page } from 'react-pdf/dist/esm/entry.webpack';
+import './App.css';
+function App() {
+
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({numPages}){
+    setNumPages(numPages);
+    setPageNumber(1);
+  }
+
+  function changePage(offSet){
+    setPageNumber(prevPageNumber => prevPageNumber + offSet);
+  }
+
+  function changePageBack(){
+    changePage(-1)
+  }
+
+  function changePageNext(){
+    changePage(+1)
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Document file="/sample.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+          <Page height="600" pageNumber={pageNumber} />
+        </Document>
+        <p> Page {pageNumber} of {numPages}</p>
+        { pageNumber > 1 && 
+        <button onClick={changePageBack}>Previous Page</button>
+        }
+        {
+          pageNumber < numPages &&
+          <button onClick={changePageNext}>Next Page</button>
+        }
+      </header>
+      <center>
+        <div>
+          <Document file="/sample.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+            {Array.from(
+              new Array(numPages),
+              (el,index) => (
+                <Page 
+                  key={`page_${index+1}`}
+                  pageNumber={index+1}
+                />
+              )
+            )}
+          </Document>
+        </div>
+      </center>
+    </div>
+  );
+}
+
+export default App;
